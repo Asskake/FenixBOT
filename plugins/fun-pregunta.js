222e18c5e2661e3dae65c1e98c2fc5bff95a74fc
@Asska <134514812+Asskake@users.noreply.github.com>
2023-11-01 06:18:40 +0700

    Update fun-pregunta.js

--- plugins/fun-pregunta.js
+++ plugins/fun-pregunta.js
@@ -1,12 +1,12 @@
 const handler = async (m, {command, text}) => m.reply(`
-*⁉️ 𝐏𝐑𝐄𝐆𝐔𝐍𝐓𝐀𝐒 ⁉️*
+*[PERTANYAAN]*
   
-*𝙿𝚁𝙴𝙶𝚄𝙽𝚃𝙰:* ${text}
-*𝚁𝙴𝚂𝙿𝚄𝙴𝚂𝚃𝙰:* ${['Si', 'Tal vez sí', 'Posiblemente', 'Probablemente no', 'No', 'Imposible'].getRandom()}
+*BERTANYA:* ${text}
+*MENJAWAB:* ${['Ya', 'Mungkin', 'Mungkin iya', 'Mungkin tidak', 'Tidak', 'Mustahil'].getRandom()}
 `.trim(), null, m.mentionedJid ? {
   mentions: m.mentionedJid,
 } : {});
-handler.help = ['pregunta <texto>?'];
+handler.help = ['apakah <texto>?'];
 handler.tags = ['kerang'];
 handler.command = /^pregunta|preguntas|apakah$/i;
 export default handler;
