df4be9ac418511f54fe7dac588992ba3ba1076f6
@Asska <134514812+Asskake@users.noreply.github.com>
2023-11-01 05:16:05 +0700

    Update buscador-lyrics.js

--- plugins/buscador-lyrics.js
+++ plugins/buscador-lyrics.js
@@ -3,7 +3,7 @@ import {getTracks} from '@green-code/music-track-data';
 import {googleImage} from '@bochilteam/scraper';
 const handler = async (m, {conn, text, usedPrefix, command}) => {
   const teks = text ? text : m.quoted && m.quoted.text ? m.quoted.text : '';
-  if (!teks) throw `*[❗𝐈𝐍𝐅𝐎❗] 𝙴𝙹𝙴𝙼𝙿𝙻𝙾 𝙳𝙴 𝚄𝚂𝙾 𝙲𝙾𝚁𝚁𝙴𝙲𝚃𝙾 𝙳𝙴𝙻 𝙲𝙾𝙼𝙰𝙽𝙳𝙾: ${usedPrefix + command} beret ojala*`;
+  if (!teks) throw `*[❗𝐈𝐍𝐅𝐎❗] Contoh penggunaan command yang benar: ${usedPrefix + command} Baoncikadap Lagu Jorok*`;
   try {
     const result = await getTracks(teks);
     const lyrics = await find_lyrics(`${result[0].artist} ${result[0].title}`);
@@ -20,11 +20,11 @@ const handler = async (m, {conn, text, usedPrefix, command}) => {
         img = await bochil.getRandom();
       }
     }
-    const textoLetra = `🎤 𝚃𝙸𝚃𝚄𝙻𝙾: *${result[0].title || ''}*\n👤 𝙰𝚄𝚃𝙾𝚁: *${result[0].artist || ''}*\n\n📃🎵 𝙻𝙴𝚃𝚁𝙰:\n${lyrics || ''}`;
+    const textoLetra = `🎤 Title: *${result[0].title || ''}*\n👤 Author: *${result[0].artist || ''}*\n\n📃🎵 Surat:\n${lyrics || ''}`;
     await conn.sendMessage(m.chat, {image: {url: img}, caption: textoLetra}, {quoted: m});
     await conn.sendMessage(m.chat, {audio: {url: result[0].preview}, fileName: `${result[0].artist} ${result[0].title}.mp3`, mimetype: 'audio/mp4'}, {quoted: m});
   } catch {
-    throw `*[❗𝐈𝐍𝐅𝐎❗] 𝙴𝚁𝚁𝙾𝚁, 𝙿𝙾𝚁 𝙵𝙰𝚅𝙾𝚁 𝚅𝚄𝙴𝙻𝚅𝙰 𝙰 𝙸𝙽𝚃𝙴𝙽𝚃𝙰𝚁𝙻𝙾*`;
+    throw `*[❗𝐈𝐍𝐅𝐎❗] Kesalahan, silakan coba lagi*`;
   }
 };
 handler.help = ['lirik', 'letra'].map((v) => v + ' <Apa>');
