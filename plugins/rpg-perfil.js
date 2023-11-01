155bd859edf70febcaf104e87492853dfa50bdd8
@Asska <134514812+Asskake@users.noreply.github.com>
2023-11-01 09:16:51 +0700

    Update rpg-perfil.js

--- plugins/rpg-perfil.js
+++ plugins/rpg-perfil.js
@@ -2,9 +2,9 @@ import {createHash} from 'crypto';
 import PhoneNumber from 'awesome-phonenumber';
 import fetch from 'node-fetch';
 const handler = async (m, {conn, usedPrefix, participants, isPrems}) => {
-  let pp = 'https://telegra.ph/file/06cc652844ea19e8aed1c.jpg';
+  let pp = 'https://telegra.ph/file/46696a4359edb50c8ba8b.jpg';
   const who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender;
-  if (!(who in global.db.data.users)) throw `El usuario que está mencionando no está registrado en mi base de datos`;
+  if (!(who in global.db.data.users)) throw `Pengguna yang Anda sebutkan tidak terdaftar di database`;
   try {
     pp = await conn.profilePictureUrl(who);
   } catch (e) {
@@ -14,12 +14,12 @@ const handler = async (m, {conn, usedPrefix, participants, isPrems}) => {
     const prem = global.prems.includes(who.split `@` [0]);
     const sn = createHash('md5').update(who).digest('hex');
     const str = `*𝙽𝙾𝙼𝙱𝚁𝙴:* ${username} ${registered ? '(' + name + ') ': ''}
-*𝙽𝚄𝙼𝙴𝚁𝙾:* ${PhoneNumber('+' + who.replace('@s.whatsapp.net', '')).getNumber('international')}
-*𝙻𝙸𝙽𝙺:* wa.me/${who.split`@`[0]}${registered ? '\n*𝙴𝙳𝙰𝙳:* ' + age + ' años' : ''}
-*𝙻𝙸𝙼𝙸𝚃𝙴:* ${limit} 𝚄𝚂𝙾𝚂
-*𝚁𝙴𝙶𝙸𝚂𝚃𝚁𝙰𝙳𝙾:* ${registered ? 'Si': 'No'}
-*𝙿𝚁𝙴𝙼𝙸𝚄𝙼:* ${premiumTime > 0 ? 'Si' : (isPrems ? 'Si' : 'No') || ''}
-*𝙽𝚄𝙼𝙴𝚁𝙾 𝙳𝙴 𝚂𝙴𝚁𝙸𝙴:* 
+*NAMA:* ${PhoneNumber('+' + who.replace('@s.whatsapp.net', '')).getNumber('international')}
+*𝙻𝙸𝙽𝙺:* wa.me/${who.split`@`[0]}${registered ? '\n*Usia:* ' + age + ' Tahun' : ''}
+*LIMIT:* ${limit}
+*REGISTER:* ${registered ? 'Ya': 'No'}
+*PREMIUM:* ${premiumTime > 0 ? 'Ya' : (isPrems ? 'Ya' : 'No') || ''}
+*NOMOR SERI:* 
 ${sn}`;
     conn.sendMessage(m.chat, {image: {url: pp}, caption: str}, {quoted: m});
   }
