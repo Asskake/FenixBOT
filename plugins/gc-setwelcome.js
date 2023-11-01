4d2eb046ca70b5748fe314c2226203c7c2af77a6
@Asska <134514812+Asskake@users.noreply.github.com>
2023-11-01 10:59:55 +0700

    Update gc-setwelcome.js

--- plugins/gc-setwelcome.js
+++ plugins/gc-setwelcome.js
@@ -1,8 +1,8 @@
 const handler = async (m, {conn, text, isROwner, isOwner}) => {
   if (text) {
     global.db.data.chats[m.chat].sWelcome = text;
-    m.reply('*[❗] PESAN SELAMAT DATANG TELAH DISETTING UNTUK GRUP INI*');
-  } else throw `*[❗] MASUKKAN PESAN SELAMAT DATANG YANG INGIN ANDA TAMBAHKAN, Example:*\n*- @user (Orang)*\n*- @group (nama grup)*\n*- @desc (deskripsi grup)*`;
+    m.reply('*PESAN SELAMAT DATANG TELAH DISETTING UNTUK GRUP INI*');
+  } else throw `*MASUKKAN PESAN SELAMAT DATANG YANG INGIN ANDA TAMBAHKAN, Example:*\n*- @user (Orang)*\n*- @group (nama grup)*\n*- @desc (deskripsi grup)*`;
 };
 handler.help = ['setwelcome <text>'];
 handler.tags = ['group'];
